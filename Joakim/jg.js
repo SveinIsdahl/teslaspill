8d223b40803ce043f78c5de857b75460f568c70b
Joakim Gjerde <joakim@Joakims-MacBook-Air.local>
2020-11-18 12:17:02 +0100

    asd

--- Joakim/jg.js
+++ Joakim/jg.js
@@ -2,7 +2,31 @@
 
 var imageOffset = 0
 
-function movecar() {
-  imageOffset += 50
-  document.getElementById("tesla").style.left = imageOffset + "px";
+function setup() {
+
+addEventListener("keypress", function movecar(e) {
+
+  if(e.key === "d") {
+    imageOffset += 50
+    document.getElementById("tesla").style.left = imageOffset + "px";
+    return;
+  }
+  else if (e.key === "a") {
+    imageOffset -= 50
+    document.getElementById("tesla").style.left = imageOffset + "px";
+    return;
+  }
+  else if (e.key === "w") {
+    imageOffset -= 50
+    document.getElementById("tesla").style.top = imageOffset + "px";
+    return;
+  }
+  else if (e.key === "s") {
+    imageOffset += 50
+    document.getElementById("tesla").style.top = imageOffset + "px";
+    return;
+  }
+}) 
+
+
 }
