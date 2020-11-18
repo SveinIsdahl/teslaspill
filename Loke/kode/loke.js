163133376850c6e98be23f259253acb761e815f7
Loke Svelland <lokesvelland@gmail.com>
2020-11-18 10:33:34 +0100

    mellom changes

--- Loke/kode/loke.js
+++ Loke/kode/loke.js
@@ -1,39 +1,89 @@
 //@ts-check
 
-class Car {
-    constructor(x, y, img) {
+class Bil {
+    /**
+     * @param {number} x
+     * @param {number} y
+     */
+    constructor(x, y) {
         this.x = x;
         this.y = y;
         this.vx = 0;
         this.vy = 0;
         this.ax = 0;
         this.ay = 0;
-        this.img = img;
         this.width = 50;
         this.height = 100;
         this.rotation = 0;
-        this.S = {};
-        this.A = {};
-        this.B = {};
-        this.C = {};
-        this.D = {};
-        this.pointAngle = atan(this.width / this.height);
-        this.radius = 25 * Math.sqrt(5);
+        this.div = ""
     }
+    render() {
+        this.x += this.vx;
+        this.vx *= 0.98;
 
-    window.onload = function () {
-        const divBane = document.getElementById("bane");
-        const divBil = document.getElementById("bil");
-        const divFjell = document.getElementById("fjell")
+        this.div.style.bottom = `${this.y}px`;
+        this.div.style.left = `${this.x}px`;
+    }
+        /**
+     * Roterer bilen rundt midtpunktet i bil-div
+     * @param {string} direction
+     */
+    turn(direction) {
+        if (direction === "right") {
+            this.rotation -= 5
+            this.div.style.transform = `rotate(${this.rotation}deg) scaleX(-1)`
+        } else {
+            this.rotation += 5
+            this.div.style.transform = `rotate(${this.rotation}deg) scaleX(-1)`
+        }
+    }
+}
 
-        document.addEventListener("keydown", drive);
+/** @type {any} */
+window.onload = function () {
+    const divBane = document.getElementById("bane");
+    const divBil = document.getElementById("bil");
+    const divFjell = document.getElementById("fjell");
+    document.addEventListener("keydown", drive);
 
-        function drive(e) {
-            if (e.key === "d") {
-                divBil
+    let keyPressed = new Set; 
+
+    window.addEventListener("keydown", (e) => {
+        keysPressed.add(e.key);
+    };
+
+    window.addEventListener("keyup", (e) => {
+        for (let i = 0; i < keysPressed.size; i++) {
+            if (keysPressed.has(e.key)) {
+                keysPressed.delete(e.key);
             }
         }
+    };
 
+    let bil = new Bil(0, 620);
+    bil.div = document.getElementById("bil");
 
-
+    function drive(e) {
+        for(e.key
+        if(e.key === "w") {
+            bil.vx += 1;
+        }
+        if(e.key === "s") {
+            bil.vx -= 1;
+        }
+        if(e.key === "a") {
+            bil.turn("right");
+        }
+        if(e.key === "d") {
+            bil.turn("left");
+        } 
     }
+
+    function animation() {
+
+
+    bil.render() 
+    requestAnimationFrame(animation);
+    }
+    animation();
+}
