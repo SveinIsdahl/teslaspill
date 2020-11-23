33573ed872efc807f50ae375e076a34d5523f255
Joakim Gjerde <joakim@Joakims-MacBook-Air.local>
2020-11-23 09:26:54 +0100

    Oppdatering

--- Joakim/jg.js
+++ Joakim/jg.js
@@ -1,29 +1,19 @@
 // @ts-check
 
-var imageOffset = 0
+var imageOffset = -20
 
 function setup() {
 
 addEventListener("keypress", function movecar(e) {
 
   if(e.key === "d") {
-    imageOffset += 50
+    imageOffset += 110
     document.getElementById("tesla").style.left = imageOffset + "px";
     return;
   }
   else if (e.key === "a") {
-    imageOffset -= 50
+    imageOffset -= 110
     document.getElementById("tesla").style.left = imageOffset + "px";
-    return;
-  }
-  else if (e.key === "w") {
-    imageOffset -= 50
-    document.getElementById("tesla").style.top = imageOffset + "px";
-    return;
-  }
-  else if (e.key === "s") {
-    imageOffset += 50
-    document.getElementById("tesla").style.top = imageOffset + "px";
     return;
   }
 }) 
